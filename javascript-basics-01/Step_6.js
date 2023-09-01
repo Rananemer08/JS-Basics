962a0785fcf2fa5035c797f3c24b37a5f6f42eb5
Rananemer08 <rananemer.cs@gmail.com>
2023-09-01 10:17:02 +0300

    Step6

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,7 @@
+let num1, num2, remainder;
+num1 = parseFloat(prompt("Enter the first number:"));
+num2 = parseFloat(prompt("Enter the second number:"));
+
+remainder = num1 % num2;
+
+alert("The remainder of dividing " + num1 + " by " + num2 + " is: " + remainder);
