529a88e86ede8a2f6a4e97fbeade158ae0edf16b
Rananemer08 <rananemer.cs@gmail.com>
2023-09-01 10:05:07 +0300

    Step5

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,6 @@
+
+let num1,num2,result;
+num1 = parseFloat(prompt("Enter the first number:"));
+num2 = parseFloat(prompt("Enter the second number:"));
+result = num1 * num2;
+alert("Result: " + result);
