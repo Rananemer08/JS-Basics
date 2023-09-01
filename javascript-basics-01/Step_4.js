2237feb2b8a8c2b8c03dd28aaf5663955de7f713
Rananemer08 <rananemer.cs@gmail.com>
2023-09-01 09:47:55 +0300

    Step4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,6 @@
+let name,surname,city;
+ name = prompt("Please enter your name:"); 
+
+surname = prompt("Please enter your surname:"); 
+city = prompt("Please enter your city:"); 
+alert("Name: " + name + "\nSurname: " + surname + "\nCity: " + city);
